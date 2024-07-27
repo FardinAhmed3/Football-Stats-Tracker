3acbad4b288577a3e80795824643a176e43beb58
FardinAhmed3 <ahmedfardin077@gmail.com>
2024-07-27 19:21:31 -0400

    Added authentication permissions

--- backend/football_app/views.py
+++ backend/football_app/views.py
@@ -1,13 +1,16 @@
+# football_app/views.py
+
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from .models import Team, Player
 from .serializers import TeamSerializer, PlayerSerializer
-import corsheaders
-
 
 class TeamViewSet(viewsets.ModelViewSet):
     queryset = Team.objects.all()
     serializer_class = TeamSerializer
+    permission_classes = [IsAuthenticated]
 
 class PlayerViewSet(viewsets.ModelViewSet):
     queryset = Player.objects.all()
     serializer_class = PlayerSerializer
+    permission_classes = [IsAuthenticated]
